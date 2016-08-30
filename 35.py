1918c51ab53baf3a7ca95f4d55645f464f888546
agatakaraskiewicz <agata.a.karaskiewicz@gmail.com>
2016-08-30 12:26:33 +0200

    carefully changed variables names; deleted unnecessary whitespaces in functions; added some comments (just for me ^^)

--- 35.py
+++ 35.py
@@ -1,8 +1,9 @@
-base = range (0, 1001)
-next = []
+base_numbers = range(0, 1001)
+devisible_numbers = []
 
-for x in base:
-    if x % 3 == 0 or x % 5 == 0:
-        next.append(x)
-
-print sum (next)
+for x in base_numbers:
+    if x % 3 == 0 or x % 5 == 0: #checks if each of numbers from range is
+                                    #devisible by 3 or 5 w/o rest
+        devisible_numbers.append(x) #if the condition is True for x, x is added
+                                    #to devisible_numbers list
+print sum(devisible_numbers)
