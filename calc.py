9d41806ee2f84d4bb179accdbcb6923a5735af75
agatakaraskiewicz <agata.a.karaskiewicz@gmail.com>
2016-10-30 23:06:55 +0100

    fixed problem with not working addition in the middle

--- calc.py
+++ calc.py
@@ -3,11 +3,11 @@ import sys
 equat = sys.argv[1:]
 joined = ''.join(equat)
 
-if joined[-1] in '0123456789':
-    joined = joined + '.0'
-elif joined[-1] == ')':
-    if joined[-2] in '0123456789':
-        joined = joined[:-1] + '.0' + joined[-1]
+if joined[0] in '0123456789':
+    joined = joined[:1] + '.0' + joined[1:]
+elif joined[0] == '(':
+    if joined[1] in '0123456789':
+        joined = joined[:2] + '.0' + joined[2:]
     else:
         print 'WTF?! Check, if you wrote everything correctly!'
 else:
