9e87510bdc97377603e79286e0a47fb8914736bb
agatakaraskiewicz <agata.a.karaskiewicz@gmail.com>
2016-10-30 22:47:02 +0100

    fixed the problem with brackets

--- calc.py
+++ calc.py
@@ -7,7 +7,7 @@ if joined[-1] in '0123456789':
     joined = joined + '.0'
 elif joined[-1] == ')':
     if joined[-2] in '0123456789':
-        joined = joined + '.0'
+        joined = joined[:-1] + '.0' + joined[-1]
     else:
         print 'WTF?! Check, if you wrote everything correctly!'
 else:
