d6b6c12ef6c4d109341785f54b48ef3b01e10504
agatakaraskiewicz <agata.a.karaskiewicz@gmail.com>
2016-10-30 22:36:46 +0100

    changed the name of varto <joined>; shortened this stupid thing

--- calc.py
+++ calc.py
@@ -1,54 +1,18 @@
 import sys
 
 equat = sys.argv[1:]
-join = ''.join(equat)
+joined = ''.join(equat)
 
-if join[-1] == '0':
-    join = join + '.0'
-elif join[-1] == '1':
-    join = join + '.0'
-elif join[-1] == '2':
-    join = join + '.0'
-elif join[-1] == '3':
-    join = join + '.0'
-elif join[-1] == '4':
-    join = join + '.0'
-elif join[-1] == '5':
-    join = join + '.0'
-elif join[-1] == '6':
-    join = join + '.0'
-elif join[-1] == '7':
-    join = join + '.0'
-elif join[-1] == '8':
-    join = join + '.0'
-elif join[-1] == '9':
-    join = join + '.0'
-elif join[-1] == ')':
-    if join[-2] == '0':
-        join = join + '.0'
-    elif join[-2] == '1':
-        join = join + '.0'
-    elif join[-2] == '2':
-        join = join + '.0'
-    elif join[-2] == '3':
-        join = join + '.0'
-    elif join[-2] == '4':
-        join = join + '.0'
-    elif join[-2] == '5':
-        join = join + '.0'
-    elif join[-2] == '6':
-        join = join + '.0'
-    elif join[-2] == '7':
-        join = join + '.0'
-    elif join[-2] == '8':
-        join = join + '.0'
-    elif join[-2] == '9':
-        join = join + '.0'
+if joined[-1] in '0123456789':
+    joined = joined + '.0'
+elif joined[-1] == ')':
+    if joined[-2] in '0123456789':
+        joined = joined + '.0'
     else:
-        print 'WTF?! Check, if you wrote everything correctly! '
+        print 'WTF?! Check, if you wrote everything correctly!'
 else:
-    print 'WTF?! Check, if you wrote everything correctly! '
-    
-count = eval(join)
+    print 'WTF?! Check, if you wrote everything correctly!'
+
+count = eval(joined)
 
 print count
