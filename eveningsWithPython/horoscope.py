ff510df42af0d3bef503ef299ed272b2d9be5c40
agatakaraskiewicz <agata.a.karaskiewicz@gmail.com>
2020-11-03 16:12:24 +0100

    A horoscope. It collects data regarding day and month of birth and outputs it

---
+++ eveningsWithPython/horoscope.py
@@ -0,0 +1,9 @@
+
+# Ask for month of birth
+birthMonth = input('What is your birth month? (1-12)')
+
+# Ask for the day of birth
+birthDay = input('Which day of the month you were born?')
+
+# Say the full birth date
+print('So you were born on ' + birthDay + '.' + birthMonth)
