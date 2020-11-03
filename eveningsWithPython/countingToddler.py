8f299c9736b51f4bc15b3765c23e0891ec82bdb3
agatakaraskiewicz <agata.a.karaskiewicz@gmail.com>
2020-11-03 20:20:46 +0100

    He does not know how to count back, so refuses in such cases

--- eveningsWithPython/countingToddler.py
+++ eveningsWithPython/countingToddler.py
@@ -2,7 +2,9 @@
 startNumber = int(input('What is the beginning number?'))
 endNumber = int(input('What is the end of our counting?'))
 
-
-while startNumber <= endNumber:
-	print(startNumber)
-	startNumber += 1
+if endNumber > startNumber:
+	while startNumber <= endNumber:
+		print(startNumber)
+		startNumber += 1
+else:
+	print('Not possible! It would be reverse... I cannot do that yet!')
