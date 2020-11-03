5a15d4503609d49921462fe8daf849233fee6d6b
agatakaraskiewicz <agata.a.karaskiewicz@gmail.com>
2020-11-03 17:59:43 +0100

    first question of the quiz; condition uses variable for comparing

---
+++ eveningsWithPython/quiz.py
@@ -0,0 +1,9 @@
+
+# question 1
+noblePrize = 'Olga Tokarczuk'
+userAnswer = input('Who got the literature Noble prize in 2019?')
+
+if userAnswer == noblePrize:
+	print('Correct!')
+else:
+	print('Nope. It was ' + noblePrize)
