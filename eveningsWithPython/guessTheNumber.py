05d3e91660ecc922c8d794132ebe9d6b21ea70d7
agatakaraskiewicz <agata.a.karaskiewicz@gmail.com>
2020-11-03 21:50:36 +0100

    Enhanced the whole solution - it was buggy. Fixed all the issues and now it should work nicely

--- eveningsWithPython/guessTheNumber.py
+++ eveningsWithPython/guessTheNumber.py
@@ -1,21 +1,32 @@
 from random import randint
 
-luckyNumber = randint(0, 10)
+# Create a random number
+luckyNumber = randint(1, 10)
 attempts = 1
+# Ask for guess
 userGuess = int(input('Guess a number between 1 and 10'))
 
+# if the user guesses right away - inform!
 if userGuess == luckyNumber:
 	print(f'Congrats! You guessed the number and it took only {attempts} attempt!')
 else:
-	while userGuess != luckyNumber and attempts < 3:
+	# As long as the guess is not correct - inform and ask again
+	while userGuess != luckyNumber:
 		print('Not this time')
-		print(f'You tried {attempts} times')
+		print(f'You tried {attempts} time(s)')
 		userGuess = int(input('Guess a number between 1 and 10'))
+		# Increment the number of attempts taken
 		attempts += 1
 
-if attempts == 3:
-	print('You failed. Try again.')
-else:
-	print(f'Congrats! You guessed the number and it took only {attempts} attempts!')	
-
-
+		#if user finds the number on second or third attempt -inform and break the loop
+		if userGuess == luckyNumber:
+			print(f'Congrats! You guessed the number and it took only {attempts} attempts!')
+			break
+		#if no success on all 3 attempts - fail and breake the loop	
+		elif userGuess != luckyNumber and attempts == 3:
+			print(f'You failed. The lucky number was: {luckyNumber}. Try again.')
+			break
+		#if no success, but there are still attempts left - continue the loop	
+		else:
+			continue
+	
