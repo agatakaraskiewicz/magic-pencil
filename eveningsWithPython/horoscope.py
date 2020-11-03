7b836d3111bda618239f80e67831be11b60c6782
agatakaraskiewicz <agata.a.karaskiewicz@gmail.com>
2020-11-03 16:23:53 +0100

    Added new feature to the horoscope - it asks for shoe size and from provided data creates a lucky number

--- eveningsWithPython/horoscope.py
+++ eveningsWithPython/horoscope.py
@@ -7,3 +7,13 @@ birthDay = input('Which day of the month you were born?')
 
 # Say the full birth date
 print('So you were born on ' + birthDay + '.' + birthMonth)
+
+# Substract birthMonth from birthDay
+subNum = int(birthDay) - int(birthMonth)
+
+# Ask for the shoe size
+shoeSize = input('What is your shoe size?')
+
+# Add shoeSize to your subNum and present the lucky number to the User
+luckyNum = subNum + int(shoeSize)
+print('So your lucky number is ' + str(luckyNum) + '.')
