9c46e7b3a833ba70cebdd8321d20f05c4961f294
agatakaraskiewicz <agata.a.karaskiewicz@gmail.com>
2020-11-03 18:06:07 +0100

    There are two Nobel prize winners so I added the second option and used OR in the condition

--- eveningsWithPython/quiz.py
+++ eveningsWithPython/quiz.py
@@ -1,9 +1,10 @@
 
 # question 1
 noblePrize = 'Olga Tokarczuk'
+secondNoblePrize = 'Peter Handke'
 userAnswer = input('Who got the literature Noble prize in 2019?')
 
-if userAnswer in noblePrize:
+if userAnswer in noblePrize or userAnswer in secondNoblePrize:
 	print('Correct!')
 else:
-	print('Nope. It was ' + noblePrize)
+	print('Nope. It was ' + noblePrize + ' and ' + secondNoblePrize)
