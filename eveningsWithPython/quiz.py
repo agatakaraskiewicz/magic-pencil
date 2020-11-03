7359984d2fdd3df9f13ee4bb3dd8c6640c298947
agatakaraskiewicz <agata.a.karaskiewicz@gmail.com>
2020-11-03 18:18:10 +0100

    rolled back to regular comparision in the question 1 to make it more precise, but added two more options for providing only surnames of the winners

--- eveningsWithPython/quiz.py
+++ eveningsWithPython/quiz.py
@@ -2,9 +2,12 @@
 # question 1
 nobelPrize = 'Olga Tokarczuk'
 secondNobelPrize = 'Peter Handke'
+alternativeNobelPrize = 'Tokarczuk'
+secondAltNobelPrize = 'Handke'
 userAnswer = input('Who got the literature Nobel prize in 2019?')
 
-if userAnswer in nobelPrize or userAnswer in secondNobelPrize:
+if userAnswer == nobelPrize or userAnswer == secondNobelPrize or userAnswer == alternativeNobelPrize or userAnswer == secondAltNobelPrize:
 	print('Correct!')
 else:
-	print('Nope. It was ' + nobelPrize + ' and ' + secondNobelPrize)
+	print('Nope. Correct answers are: ' + nobelPrize + ' or ' + secondNobelPrize + ' or ' + alternativeNobelPrize + ' or ' + secondAltNobelPrize)
+	
