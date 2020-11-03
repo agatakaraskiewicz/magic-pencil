864156f3d85049064767fb93549af60e570fae74
agatakaraskiewicz <agata.a.karaskiewicz@gmail.com>
2020-11-03 18:02:17 +0100

    Now instead of regular comparing it uses logic operator IN; It will accept everything which is included in the noblePrize var

--- eveningsWithPython/quiz.py
+++ eveningsWithPython/quiz.py
@@ -3,7 +3,7 @@
 noblePrize = 'Olga Tokarczuk'
 userAnswer = input('Who got the literature Noble prize in 2019?')
 
-if userAnswer == noblePrize:
+if userAnswer in noblePrize:
 	print('Correct!')
 else:
 	print('Nope. It was ' + noblePrize)
