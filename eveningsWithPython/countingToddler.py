beaa200956d649c57b35cf403516c025191f011a
agatakaraskiewicz <agata.a.karaskiewicz@gmail.com>
2020-11-03 20:15:37 +0100

    Excercise with while and printing numbers in some range

---
+++ eveningsWithPython/countingToddler.py
@@ -0,0 +1,8 @@
+
+startNumber = int(input('What is the beginning number?'))
+endNumber = int(input('What is the end of our counting?'))
+
+
+while startNumber <= endNumber:
+	print(startNumber)
+	startNumber += 1
