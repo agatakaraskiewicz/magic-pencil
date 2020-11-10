6d0f2ef720aeaff507c1cb998ff7c1b82a16b179
agatakaraskiewicz <agata.a.karaskiewicz@gmail.com>
2020-11-10 19:54:30 +0100

    added while loop; counting points; added possibility to say how many wins are needed to win the battle

--- eveningsWithPython/stonePaperScissorsGame.py
+++ eveningsWithPython/stonePaperScissorsGame.py
@@ -8,34 +8,51 @@ from random import randint
 2 beats 3
 3 beats 1
 """
+points = 0
+userPoints = 0
+
+howManyWins = int(input('How many wins?'))
 
 #1. Create some randomized number (1,3), which will be the comp attack
-attack = randint(1,3)
-
-#2. Ask for the RPS input
-userAttack = int(input('What is your attack? Input 1 for Paper, 2 for Stone or 3 for Scissors'))
 
 #3. Compare the created values and decide, who won
-if userAttack == 1:
-	if attack == 1:
-		print(f'Tie!')
-	elif attack == 2:
-		print(f'You won!')
+while points < howManyWins and userPoints < howManyWins:
+	userAttack = int(input('What is your attack? Input 1 for Paper, 2 for Stone or 3 for Scissors'))
+	attack = randint(1,3)
+	if userAttack == attack:
+		print (f'Tie! No one gets the point!')
+		continue
+	elif userAttack == 1:
+		if attack == 2:
+			print(f'You won!')
+			userPoints += 1
+			continue
+		else:
+			print(f'You loose!')
+			points += 1
+			continue
+	elif userAttack == 2:
+		if attack == 3:
+			print(f'You won!')
+			userPoints += 1
+			continue
+		else:
+			print(f'You loose!')
+			points += 1
+			continue
+	elif userAttack == 3:
+		if attack == 1:
+			print(f'You won!')
+			userPoints += 1
+			continue
+		else:
+			print(f'You loose!')
+			points += 1
+			continue
 	else:
-		print(f'You loose!')
-elif userAttack == 2:
-	if attack == 2:
-		print(f'Tie!')
-	elif attack == 3:
-		print(f'You won!')
-	else:
-		print(f'You loose!')
-elif userAttack == 3:
-	if attack == 3:
-		print(f'Tie!')
-	elif attack == 1:
-		print(f'You won!')
-	else:
-		print(f'You loose!')
+		print('You were supposed to provide 1-3 number... Try again')
+
+if points > userPoints:
+	print('Computer won the battle!')	
 else:
-	print('You were supposed to provide 1-3 number... Try again')
+	print('You won the battle!')
