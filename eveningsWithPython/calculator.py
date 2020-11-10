2d59692634845b484110acc4486888f937d295d8
agatakaraskiewicz <agata.a.karaskiewicz@gmail.com>
2020-11-10 20:26:32 +0100

    added functionality to repeat the whole process 3 times

--- eveningsWithPython/calculator.py
+++ eveningsWithPython/calculator.py
@@ -1,20 +1,25 @@
-def calculate(first, second, math):
-	if math == 'sum':
-		return first + second
-	elif math == 'minus':
-		return first - second
-	elif math == 'times':
-		return first * second
-	elif math == 'divide':
-		return first / second
-	else:
-		print('There is no such operation. Try again')
+def runFunction():
+	def calculate(first, second, math):
+		if math == 'sum':
+			return first + second
+		elif math == 'minus':
+			return first - second
+		elif math == 'times':
+			return first * second
+		elif math == 'divide':
+			return first / second
+		else:
+			print('There is no such operation. Try again')
 
-firstNmbr = int(input('What is the first number?'))
-secondNmbr = int(input('What is the second number?'))
+	firstNmbr = int(input('What is the first number?'))
+	secondNmbr = int(input('What is the second number?'))
 
-mathOperation = input('What do I do with them? (sum, minus, times or divide)')
+	mathOperation = input('What do I do with them? (sum, minus, times, divide)')
 
-result = calculate(firstNmbr, secondNmbr, mathOperation)
+	result = calculate(firstNmbr, secondNmbr, mathOperation)
 
-print('The result is: ' + str(result))
+	print('The result is: ' + str(result))
+	
+runFunction()
+runFunction()
+runFunction()	
