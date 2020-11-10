c4c2a7a69182085f0e449f9a557ba7d16e8c863e
agatakaraskiewicz <agata.a.karaskiewicz@gmail.com>
2020-11-10 20:18:44 +0100

    A simple calculator with very simple function

---
+++ eveningsWithPython/calculator.py
@@ -0,0 +1,20 @@
+def calculate(first, second, math):
+	if math == 'sum':
+		return first + second
+	elif math == 'minus':
+		return first - second
+	elif math == 'times':
+		return first * second
+	elif math == 'divide':
+		return first / second
+	else:
+		print('There is no such operation. Try again')
+
+firstNmbr = int(input('What is the first number?'))
+secondNmbr = int(input('What is the second number?'))
+
+mathOperation = input('What do I do with them? (sum, minus, times or divide)')
+
+result = calculate(firstNmbr, secondNmbr, mathOperation)
+
+print('The result is: ' + str(result))
